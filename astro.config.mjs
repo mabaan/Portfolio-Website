1d62e128d70f1ddd9d9f0af301c4f227bf2538bc
68freeworld <b00097219@aus.edu>
2026-02-13 05:59:06 +0400

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  output: "static",
+  output: "server",
+  adapter: vercel(),
   integrations: [tailwind()],
 });
